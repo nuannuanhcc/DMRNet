eb208f349f030b7c5d465d63420063f4fbe6aa79
hcc <hcc@hust.edu.cn>
2022-02-02 17:38:13 +0800

    erun

--- erun.sh
+++ erun.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-# python ./mmdetection/tools/test.py ./configs/reppoints_moment_r50_fpn_1x.py work_dirs/dmrnet/latest.pth
-./mmdetection/tools/dist_test.sh ./configs/reppoints_moment_r50_fpn_1x.py \
-                work_dirs/dmrnet/latest.pth 8
+export DIR_NAME="reppoints"
+python3 ./mmdetection/tools/test.py ./configs/reppoints_moment_r50_fpn_1x.py ./work_dirs/$DIR_NAME/latest.pth --work_dir './work_dirs/'$DIR_NAME''
+
+./mmdetection/tools/dist_test.sh ./configs/reppoints_moment_r50_fpn_1x.py ./work_dirs/$DIR_NAME/latest.pth 4 --work_dir './work_dirs/'$DIR_NAME''
